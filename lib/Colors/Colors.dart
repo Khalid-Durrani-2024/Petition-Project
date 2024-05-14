907006f046d01d3ae947d0de80708a000b3bda56
Khalid Durrani <khaliddurrani113@gmail.com>
2024-05-14 17:04:33 +0430

    Log In Screen UI Completed also colors has been sitted

---
+++ lib/Colors/Colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class CustomColors {
+  var backgroundColor = const Color.fromARGB(255, 79, 66, 114);
+  var buttonColor = const Color.fromARGB(255, 97, 65, 187);
+  var textFieldColor = const Color.fromARGB(255, 112, 102, 126);
+  var helperWhiteColor = const Color.fromARGB(255, 217, 217, 217);
+  var helperBlackColor = const Color.fromARGB(255, 0, 0, 0);
+}
--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,15 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:petition/Colors/Colors.dart';
 
-class Login extends StatefulWidget {
+CustomColors _colors = CustomColors(); //Colors Class Object
+
+class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: _colors.backgroundColor,
+      body: LoginForm(),
+    );
+  }
 }
 
-class _LoginState extends State<Login> {
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.center,
+      child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                //Email Field
+                decoration: BoxDecoration(
+                    color: _colors.textFieldColor,
+                    borderRadius: BorderRadius.circular(22)),
+                width: width / 2,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter Email Address';
+                    } else if (!value.contains('@')) {
+                      return 'Enter a Valid Email Address including "@"';
+                    } else if (!value.contains('.')) {
+                      return 'Enter a Valid Email Address including "."';
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(22)),
+                      labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w100),
+                      label: Text('Email')),
+                ),
+              ),
+              SizedBox(
+                height: height / 30,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: _colors.textFieldColor,
+                    borderRadius: BorderRadius.circular(22)),
+                width: width / 2,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Password Missing';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(22)),
+                      labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w100),
+                      label: Text('Password')),
+                ),
+              ),
+              SizedBox(
+                height: height / 30,
+              ),
+              InkWell(
+                //Log In Button
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(22),
+                      color: _colors.buttonColor),
+                  width: width / 5,
+                  height: height / 15,
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
