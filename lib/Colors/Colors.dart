a6023dda7f66255403542f6c677dc578c85c64f3
Khalid Durrani <khaliddurrani113@gmail.com>
2024-06-12 21:37:34 +0430

    Theme Changed

--- lib/Colors/Colors.dart
+++ lib/Colors/Colors.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  var backgroundColor = const Color.fromARGB(255, 79, 66, 114);
-  var buttonColor = const Color.fromARGB(255, 97, 65, 187);
-  var textFieldColor = const Color.fromARGB(255, 112, 102, 126);
+  // var backgroundColor = const Color.fromARGB(255, 79, 66, 114);
+  // var buttonColor = const Color.fromARGB(255, 97, 65, 187);
+  // var textFieldColor = const Color.fromARGB(255, 112, 102, 126);
+  // var helperWhiteColor = const Color.fromARGB(255, 217, 217, 217);
+  // var helperBlackColor = const Color.fromARGB(255, 0, 0, 0);
+
+  var backgroundColor = const Color(0xff388697);
+  var buttonColor = const Color(0xffCC2936);
+  var textFieldColor = const Color(0xff08415C);
   var helperWhiteColor = const Color.fromARGB(255, 217, 217, 217);
   var helperBlackColor = const Color.fromARGB(255, 0, 0, 0);
+
+  // var backgroundColor = const Color(0xffD6E4E5);
+  // var buttonColor = const Color(0xff4A7174);
+  // var textFieldColor = const Color(0xffEB6541);
+  // var helperWhiteColor = const Color.fromARGB(255, 217, 217, 217);
+  // var helperBlackColor = const Color.fromARGB(255, 0, 0, 0);
+
+  // var backgroundColor = const Color(0xff94B49F);
+  // var buttonColor = const Color(0xffCEE5D0);
+  // var textFieldColor = const Color(0xffFCF8E8);
+  // var helperWhiteColor = const Color.fromARGB(255, 217, 217, 217);
+  // var helperBlackColor = const Color.fromARGB(255, 0, 0, 0);
 }
 
 CustomColors colors = CustomColors(); //Colors Class Object
